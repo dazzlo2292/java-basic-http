bd275f70e89cab1026b6ff6fc36f0a37228776f6
Nikolay Shatskikh <nikolay.shatskikh@edna.ru>
2024-07-22 14:59:54 +0300

    add method disconnect()

--- Server/src/main/java/ru/otus/java/basic/http/RequestHandler.java
+++ Server/src/main/java/ru/otus/java/basic/http/RequestHandler.java
@@ -9,12 +9,14 @@ import java.util.concurrent.ExecutorService;
 public class RequestHandler {
     private final byte[] buffer;
     private final Dispatcher dispatcher;
+    private final Socket connection;
     private final OutputStream out;
     private final InputStream in;
 
     public RequestHandler(ExecutorService pool, Socket connection) throws IOException {
         this.buffer = new byte[8192];
         this.dispatcher = new Dispatcher();
+        this.connection = connection;
         this.out = connection.getOutputStream();
         this.in = connection.getInputStream();
 
@@ -30,28 +32,32 @@ public class RequestHandler {
             } catch (IOException e) {
                 e.printStackTrace();
             } finally {
-                if (in != null) {
-                    try {
-                        in.close();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-
-                if (out != null) {
-                    try {
-                        out.close();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-
-                try {
-                    connection.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+                disconnect();
             }
         });
+    }
+
+    private void disconnect() {
+        if (in != null) {
+            try {
+                in.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        if (out != null) {
+            try {
+                out.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        try {
+            connection.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
